bd81d152863b3cc15bfcc5d75d31a1c16b13ed1a
dmitry <ggbox@mail.ru>
2024-11-17 00:34:41 +0400

    remove unused files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
 
-import Canvas from "../canvas/Canvas";
 // import image from './battleBackground.png'
 
 const App = () => (
     <div className='App'>
         <h1>React and TypeScript App!</h1>
-        <Canvas />
     </div>
 );
 
